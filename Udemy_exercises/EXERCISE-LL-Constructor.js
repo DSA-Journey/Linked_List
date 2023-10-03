c4f504f92b8e478c6a5c89edb6bc92c6e6ab21ad
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-10-03 07:48:34 +0300

    Solve challenge

--- Udemy_exercises/EXERCISE-LL-Constructor.js
+++ Udemy_exercises/EXERCISE-LL-Constructor.js
@@ -5,7 +5,20 @@
 //                       //
 ///////////////////////////
 
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    };
+};
+
 class LinkedList {
+    constructor(value){
+        const newNode = new Node(value)
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1
+    }
 	// WRITE LL CONSTRUCTOR HERE //
 	//                           //
 	//                           //
